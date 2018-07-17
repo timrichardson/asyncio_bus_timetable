54f4ba962fef38be0545602a44b13adc0135d82d
Tim Richardson <tim@growthpath.com.au>
2018-07-17 11:00:47 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Example Package
 
-This is a simple example package. You can use
-[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
-to write your content.
+This is a simple beginner's project with aiohttp.
+It is a basic websocket server, incuding jinja2 templating, and non-async code processing in the background and communicating message to be sent toe websocket clients.
--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='asyncio_bus_timetable',
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Tim Richardson",
+    author_email="tim@tim-richardson.net",
+    description="Simple, beginner aiohttp project fetching PTV bus times",
     long_description='long_description',
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/timrichardson/asyncio_bus_timetable",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
